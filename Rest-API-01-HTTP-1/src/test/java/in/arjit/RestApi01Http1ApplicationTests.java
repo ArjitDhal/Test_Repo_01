a71300b8172c4704d09eff2ff9d6d90ca16ca8d4
Arjit Dhal <arjitdhal2000@gmail.com>
2024-09-18 22:54:37 +0530

    spring annotation removed

--- Rest-API-01-HTTP-1/src/test/java/in/arjit/RestApi01Http1ApplicationTests.java
+++ Rest-API-01-HTTP-1/src/test/java/in/arjit/RestApi01Http1ApplicationTests.java
@@ -3,7 +3,7 @@ package in.arjit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+ 
 class RestApi01Http1ApplicationTests {
 
 	@Test
